56bf57a3c37988ae623409046f541a7b50aa9f80
dusklight00 <r.rahul.developer@gmail.com>
2024-10-02 13:33:50 +0530

    extra details from the unsplash api added

--- app/photos/[photoId]/page.tsx
+++ app/photos/[photoId]/page.tsx
@@ -55,6 +55,71 @@ const Photo = async ({ params: { photoId } }: Params) => {
               />
             )}
           </div>
+          <div>
+            <h2 className="text-2xl font-bold mb-4">{photoData?.user.name}</h2>
+            <Image
+              src={photoData?.user.profile_image.medium}
+              alt={photoData?.user.name}
+              width={80}
+              height={80}
+              className="rounded-full"
+            />
+            <p className="mt-3">
+              <strong>Bio :</strong> {photoData?.user.bio}
+            </p>
+            <p>
+              <strong>Unsplash Account :</strong>{" "}
+              <a className="text-blue-600" href={photoData?.user.links.html}>
+                {photoData?.user.links.html}
+              </a>
+            </p>
+            <p>
+              {" "}
+              <strong>Total Likes :</strong>
+              {photoData?.user.total_likes}
+            </p>
+            <p>
+              {" "}
+              <strong>Total Photos :</strong>
+              {photoData?.user.total_photos}
+            </p>
+            <p>
+              {" "}
+              <strong>Total Collections :</strong>
+              {photoData?.user.total_collections}
+            </p>
+            <p>
+              {" "}
+              <strong>Total Photos :</strong>
+              {photoData?.user.total_photos}
+            </p>
+
+            <p>
+              {" "}
+              <strong> Description : </strong>
+              {photoData?.description}
+            </p>
+            <p>
+              {" "}
+              <strong>Created : </strong>
+              {formatDate(photoData?.created_at)}
+            </p>
+            <p>
+              {" "}
+              <strong>Last Update : </strong>
+              {formatDate(photoData?.updated_at)}
+            </p>
+            <p>
+              {" "}
+              <strong>Total Views : </strong>
+              {photoData?.views}
+            </p>
+            <p>
+              {" "}
+              <strong>Downloads : </strong>
+              {photoData?.downloads}
+            </p>
+          </div>
         </div>
       </div>
     </>
