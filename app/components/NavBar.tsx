1a5b972498a3947322dc7a9b6335e8f6c905f8f6
dusklight00 <r.rahul.developer@gmail.com>
2024-10-02 13:33:28 +0530

    search bar functionality completed

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,7 +7,20 @@ const NavBar = () => {
     <div className="p-2 border-b border-solid border-gray-400">
       <div className="navbar bg-base-100 flex justify-between">
         <div className="">
+          {/* <Link href="/">
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/next.svg"
+              alt="Next.js Logo"
+              width={120}
+              height={37}
+              priority
+            />
+          </Link> */}
           <div className="text-3xl font-bold m-4">Photo Gallery</div>
+        </div>
+        <div className="flex justify-center mx-6">
+          <Search />
         </div>
       </div>
     </div>
